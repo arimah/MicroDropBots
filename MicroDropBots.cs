a7ad5efd98bb05ce825ab709c7036817dff1733b
Arimah <arimah42@gmail.com>
2021-04-14 14:43:24 +0200

    Initial commit!

---
+++ MicroDropBots.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using BepInEx;
+using R2API.Utils;
+using RoR2;
+using ItemCatalog = On.RoR2.ItemCatalog;
+
+namespace Arimah
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.arimah.MicroDropBots", "MicroDropBots", "1.0.0")]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class MicroDropBots : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            ItemCatalog.SetItemDefs += (orig, items) => {
+                var microbots = items.First(i => i.name == "CaptainDefenseMatrix");
+                if (microbots)
+                {
+                    microbots.tags = new[] {
+                        ItemTag.Any,
+                        ItemTag.AIBlacklist,
+                        ItemTag.Utility,
+                    };
+                    Logger.LogMessage("Defensive Microbots is now findable");
+                }
+                else
+                {
+                    Logger.LogWarning("Could not locate Defensive Microbots item");
+                }
+                orig(items);
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# MicroDropBots
+
+This mod adds [Defensive Microbots][] as a regular item that can be found in the game world. With this mod, the item can be found in chests and the last Cell Vent, purchased from cauldrons, and selected with Artifact of Command. Captain will continue to spawn with it by default.
+
+**Network compatibility:** In multiplayer, only the host needs the mod.
+
+## Support
+
+If the mod fails to work, you can [open a GitHub issue][issue]. You can also contact me on Discord at Arimah#0001. I am _not_ in any Risk of Rain modding community.
+
+## Installation
+
+Use a mod manager such as [Thunderstore Mod Manager][r2mm] or copy `MicroDropBots.dll` to your plugins folder.
+
+## License
+
+The mod is licensed under the MIT license. Please see the license text for the full terms.
+
+[Defensive Microbots]: https://riskofrain2.fandom.com/wiki/Defensive_Microbots
+[issue]: https://github.com/arimah/MicroDropBots/issues/new
+[r2mm]: https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager
